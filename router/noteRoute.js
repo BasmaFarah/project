0993d38e36ba0c5c17039040ed5a2def8794c55b
BasmaFarah <djendlibasmafarah@gmail.com>
2026-09-06 11:11:58 +0100

    get note route

--- router/noteRoute.js
+++ router/noteRoute.js
@@ -1,7 +1,8 @@
 const express =require("express")
-
-const{postNote,getNotes}=require("../controllers/noteController")
+const {authentication}=require("../auth/authentication")
+const{postNote,getNotes}=require("../controllers/noteController");
+const { authentication } = require("../auth/authentication");
 const router = express.Router()
 router.post("/add",postNote);
-router.get("/all",getNotes);
+router.get("/all",authentication,getNotes);
 module.exports= {router};
